41882296c07b3f13df17c296d40a73237b3af34b
Oksana Nedilko <nedilko7b@gmail.com>
2023-03-03 15:03:18 +0200

    init commit

---
+++ bin/dart_application_lec2.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_lec2/main.dart' as dart_application_lec2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_lec2.calculate()}!');
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  print(calculate());
+}
+
+int calculate() {
+  return 6 * 7;
+}
